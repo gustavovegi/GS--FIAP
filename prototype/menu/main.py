a5bbbff5af48c8b47d71c014fe87bd43ef01e1b0
CEPIZIN~ <lauramathias03@gmail.com>
2023-11-18 16:55:48 -0300

    v.1

--- prototype/menu/main.py
+++ prototype/menu/main.py
@@ -3,34 +3,35 @@ import random
 import json
 import math
 
-#escrevendo com arquivo json
-def write_Json(file_path,data):
+# escrevendo com arquivo json
+def write_Json(file_path, data):
     with open(file_path, 'w') as file:
-        json.dump(data,file)
+        json.dump(data, file)
     file.close()
+
 def read_json(doc):
     with open(doc, "r") as file:
         data = json.load(file)
     file.close()
     return data
 
-#Validando campos
+# Validando campos
+
 def valid_email():
-        email = input("E-mail:")
-        if not '@' in email:
-            print("email invalido")
-            valid_email()
-        return email
+    email = input("E-mail:")
+    if not '@' in email:
+        print("email invalido")
+        valid_email()
+    return email
 
 def limpa_dado(campo):
     resposta = ''
     campo = str(campo)
     for i in campo:
-        if ( i.isnumeric()):
+        if (i.isnumeric()):
             resposta += i
     return resposta
 
-          
 def valid_number(campo, rule):
     var = None
     while True:
@@ -43,26 +44,31 @@ def valid_number(campo, rule):
             print(f"{campo} invalido")
     print(var)
     return var
+def valid_resp(campo):
+    while True:
+        resposta = input(f"{campo} (s/n): ").lower()
+        if resposta == 's' or resposta == 'n':
+            return resposta
+        else:
+            print("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.")
 
-
-#OPÇÃO UM
+# OPÇÃO UM
 def cadastras_user():
-
     nome_user = input("Nome Completo:")
-    CPF = valid_number("CPF",11)
+    CPF = valid_number("CPF", 11)
     email = valid_email()
-    number_phone = valid_number("Telefone",11)
+    number_phone = valid_number("Telefone", 11)
 
     user_info = {
         "Nome Completo": nome_user,
         "E-mail": email,
-          "Celular": number_phone,
+        "Celular": number_phone,
         "CPF": CPF,
     }
     write_Json("../json/user.json", user_info)
     print("usuário cadastrado com sucesso!!")
 
-#OPÇÃODOIS
+# OPÇÃODOIS
 def check_symptoms_urgency(symptoms, urgency_data):
     for category in urgency_data["categories"]:
         if any(symptom in category["symptoms"] for symptom in symptoms):
@@ -70,22 +76,19 @@ def check_symptoms_urgency(symptoms, urgency_data):
     return "Não classificado"
 
 def anamnese_on():
-    #se o json do cadastro estiver sem nada pedir para o usuário se cadastrar
-    #configurar algumas repostas para não ter erro dos usuários ( valida-la)
-
     day = datetime.datetime.now()
     day_string = day.strftime("%Y-%m-%d %H:%M:%S")
     queixa_principal = input("Queixa principal do paciente:")
-    dias_queixa= input("A quanto tempo vem sentindo os sintomas:")
+    dias_queixa = input("A quanto tempo vem sentindo os sintomas:")
 
     # História médica
-    doencas_cronicas = input("Você tem alguma doença crônica? ")
-    cirurgias = input("Você já fez alguma cirurgia? ")
-    alergias = input("Você tem alguma alergia? ")
-    medicamentos = input("Você toma algum medicamento? ")
-    fumar = input("Você fuma? ")
-    beber = input("Você bebe? ")
-    historico_familiar = input("Você tem algum histórico familiar de doenças? ")
+    doencas_cronicas = valid_resp("Você tem alguma doença crônica? ")
+    cirurgias = valid_resp("Você já fez alguma cirurgia? ")
+    alergias = valid_resp("Você tem alguma alergia? ")
+    medicamentos = valid_resp("Você toma algum medicamento? ")
+    fumar = valid_resp("Você fuma? ")
+    beber = valid_resp("Você bebe? ")
+    historico_familiar =valid_resp("Você tem algum histórico familiar de doenças? ")
 
     anamnese = {
         "Hora do preenchimento da ficha": day_string,
@@ -100,23 +103,35 @@ def anamnese_on():
         "Histórico familiar": historico_familiar
     }
     urgency_data = read_json('../json/sitomas.json')
-    patient_symptoms = [queixa_principal, doencas_cronicas, cirurgias, alergias, medicamentos, fumar, beber,historico_familiar]
+    patient_symptoms = [queixa_principal, doencas_cronicas, cirurgias, alergias, medicamentos, fumar, beber,
+                        historico_familiar]
     urgency_category = check_symptoms_urgency(patient_symptoms, urgency_data)
     print(f"A categoria de urgência é: {urgency_category}")
     write_Json("../json/anamnese.json", anamnese)
 
-#OPAÇÃOTRÊS
+# OPAÇÃOTRÊS
 def calculate_distance(latitude1, longitude1, latitude2, longitude2):
     earth_radius_km = 6371.01
     distance_in_km = earth_radius_km * math.acos(
         math.sin(math.radians(latitude1)) * math.sin(math.radians(latitude2)) +
-        math.cos(math.radians(latitude1)) * math.cos(math.radians(latitude2)) * math.cos(math.radians(longitude1 - longitude2))
+        math.cos(math.radians(latitude1)) * math.cos(math.radians(latitude2)) * math.cos(
+            math.radians(longitude1 - longitude2))
     )
     return distance_in_km
+
+# OPÇÃO TRÊS
 def get_hospital():
     latitude = -23.644909
     longitude = -46.636527
+
     try:
+        anamnese_data = read_json('../json/anamnese.json')
+        if not anamnese_data:
+            print("Você precisa preencher a ficha de anamnese antes de escolher um hospital.")
+            anamnese_on()
+            return None
+
+        # Se o usuário tem uma ficha de anamnese, continua com a escolha do hospital
         hospital_data = read_json('../json/hospital.json')
         hospitals = []
         for hospital in hospital_data:
@@ -149,18 +164,18 @@ def get_hospital():
     except Exception as err:
         print(err)
         return None
-#Opção 4
+
+# Opção 4
 def encerrar_programa():
     print("Encerrando...")
     exit()
     return
 
-
 # Configuração do menu
 opcoes_menu = {
     "1": cadastras_user,
     "2": anamnese_on,
-    "3":get_hospital,
+    "3": get_hospital,
     "4": encerrar_programa
 }
 
